f6953f2773947fa7bf8931ee467b802e49ccf496
Dario Lodeiros <dario@aloxa.eu>
2017-01-27 18:38:09 +0100

    primer commit

--- aloxa_vemo/models/inherit_mail_notification.py
+++
@@ -1,8 +0,0 @@
-from openerp import models, api
-
-class FooterlessNotification(models.Model):
-    _inherit = 'mail.notification'
-
-    @api.model
-    def get_signature_footer(self, user_id, res_model=None, res_id=None, context=None, user_signature=True):
-        return ""
