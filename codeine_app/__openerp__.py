ee26caa73ca851bfdb6a03796539dad029f61e54
Jose Luis <osotranquilo@gmail.com>
2017-02-28 20:30:23 +0100

    App CodeIne uploaded

---
+++ codeine_app/__openerp__.py
@@ -0,0 +1,7 @@
+{               
+'name': 'codeINE  Application',               
+'description':  'Management of the INE codes.',                 
+'author':   'Jose Luis Algara',              
+'application':  True,
+'data': ['codeine_view.xml','data/codeine.task.csv'],
+} 
