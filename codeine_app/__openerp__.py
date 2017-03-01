62cd78d19016ec07706cbf400f61b6fe278e4e6c
Jose Luis <osotranquilo@gmail.com>
2017-03-01 13:29:26 +0100

    Changes in filters and icon

--- codeine_app/__openerp__.py
+++ codeine_app/__openerp__.py
@@ -1,7 +1,10 @@
 {               
-'name': 'codeINE  Application',               
-'description':  'Management of the INE codes.',                 
-'author':   'Jose Luis Algara',              
+'name': 'codeINE  Application',
+'summary': "Management of the INE codes",               
+'description':  'Management of the INE codes. The codes for the INE should not be modified by the users.',                 
+'author':   'Jose Luis Algara',
+'website': "http://www.aldahotels.com",   
+'version': '9.0.1.0.0',           
 'application':  True,
 'data': ['codeine_view.xml','data/codeine.task.csv'],
 } 
