9d34b1e8ee68e57cc8f0e3ffa82767332284e187
Jose Luis Algara <osotranquilo@gmail.com>
2017-03-06 18:39:48 +0100

    Change default values

--- inherit_company/models/inherit_company_model.py
+++ inherit_company/models/inherit_company_model.py
@@ -5,10 +5,9 @@ class Inherit_res_company(models.Model):
     _inherit = 'res.company'
     tourism = fields.Char('Tourism number')
     category = fields.Char('Hotel Category')
-    rooms = fields.Integer('Rooms Available')
-    seats = fields.Integer('Beds available')
-    permanentstaff = fields.Integer('Permanent Staff')
-    eventualstaff = fields.Integer('Eventual Staff')
-    police = fields.Char('Police number',
-        size=10)
+    rooms = fields.Integer('Rooms Available',default=0)
+    seats = fields.Integer('Beds available',default=0)
+    permanentstaff = fields.Integer('Permanent Staff',default=0)
+    eventualstaff = fields.Integer('Eventual Staff',default=0)
+    police = fields.Char('Police number',size=10)
     
