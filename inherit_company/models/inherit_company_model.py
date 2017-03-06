456a231b35d169c21feda665a916a3d74164b61a
Jose Luis Algara <osotranquilo@gmail.com>
2017-03-06 18:50:30 +0100

    Add help values

--- inherit_company/models/inherit_company_model.py
+++ inherit_company/models/inherit_company_model.py
@@ -3,11 +3,11 @@ from openerp import models, fields, api
 
 class Inherit_res_company(models.Model):
     _inherit = 'res.company'
-    tourism = fields.Char('Tourism number')
-    category = fields.Char('Hotel Category')
-    rooms = fields.Integer('Rooms Available',default=0)
-    seats = fields.Integer('Beds available',default=0)
-    permanentstaff = fields.Integer('Permanent Staff',default=0)
-    eventualstaff = fields.Integer('Eventual Staff',default=0)
-    police = fields.Char('Police number',size=10)
+    tourism = fields.Char('Tourism number', help='Registration number in the Ministry of Tourism. Used for INE statistics.')
+    category = fields.Char('Hotel Category', help='Hotel category in the Ministry of Tourism. Used for INE statistics.')
+    rooms = fields.Integer('Rooms Available',default=0, help='Used for INE statistics.')
+    seats = fields.Integer('Beds available',default=0, help='Used for INE statistics.')
+    permanentstaff = fields.Integer('Permanent Staff',default=0, help='Used for INE statistics.')
+    eventualstaff = fields.Integer('Eventual Staff',default=0, help='Used for INE statistics.')
+    police = fields.Char('Police number',size=10, help='Used to generate the name of the file that will be given to the police.')
     
